2a25f1baa7cb0c8d2a32154fc5070b6a8ec40c20
Yoshiyuki Koyanagi <moutend@gmail.com>
2024-11-28 20:41:04 +0900

    Create project

---
+++ NearbyInteractionWithCustomServer/NearbyInteractionWithCustomServerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearbyInteractionWithCustomServerApp.swift
+//  NearbyInteractionWithCustomServer
+//
+//  Created by Yoshiyuki Koyanagi on 2024/11/28.
+//
+
+import SwiftUI
+
+@main
+struct NearbyInteractionWithCustomServerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
